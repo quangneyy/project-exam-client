aa2d25ad35f7852421361d96878e24a4f1fbe311
QuangNey <quangney12602@gmail.com>
2022-08-21 12:40:22 +0700

    connect-api-delete-user

--- src/components/Admin/Content/ModalDeleteUser.js
+++ src/components/Admin/Content/ModalDeleteUser.js
@@ -1,37 +1,49 @@
 import React, { useState } from 'react';
 import Button from 'react-bootstrap/Button';
 import Modal from 'react-bootstrap/Modal';
+import { deleteUser } from '../../../services/apiService';
+import { toast } from 'react-toastify';
 
 const ModalDeleteUser = (props) => {
-  const {show, setShow, dataDelete} = props;
+    const {show, setShow, dataDelete} = props;
 
-  const handleClose = () => setShow(false);
+    const handleClose = () => setShow(false);
 
-  const handleSubmitDeleteUser = () => {
-      alert("me");
-  }
-  return (
-    <>
-        <Modal 
-            show={show} 
-            onHide={handleClose}
-            backdrop="static"
-        >
-            <Modal.Header closeButton>
-                <Modal.Title>Confirm Delete the User?</Modal.Title>
-            </Modal.Header>
-            <Modal.Body>Are you sure to delete this use. email = <b>{dataDelete && dataDelete.email ? dataDelete.email : ""}</b></Modal.Body>
-            <Modal.Footer>
-                <Button variant="secondary" onClick={handleClose}>
-                    Cancel
-                </Button>
-                <Button variant="primary" onClick={()=> handleSubmitDeleteUser()}>
-                    Confirm
-                </Button>
-            </Modal.Footer>
-        </Modal>
-    </>
-  );
+    const handleSubmitDeleteUser = async () => {
+        let data = await deleteUser(dataDelete.id);
+        if (data && data.EC === 0) {
+            toast.success(data.EM);
+            handleClose();
+            await props.fetchListUsers();
+        }
+
+        if (data && data.EC !== 0) {
+            toast.error(data.EM);
+        }
+    }
+    
+    return (
+        <>
+            <Modal 
+                show={show} 
+                onHide={handleClose}
+                backdrop="static"
+            >
+                <Modal.Header closeButton>
+                    <Modal.Title>Confirm Delete the User?</Modal.Title>
+                </Modal.Header>
+                <Modal.Body>Are you sure to delete this use. email = <b>{dataDelete && dataDelete.email ? dataDelete.email : ""}</b></Modal.Body>
+                <Modal.Footer>
+                    <Button variant="secondary" onClick={handleClose}>
+                        Cancel
+                    </Button>
+                    <Button variant="primary" onClick={()=> handleSubmitDeleteUser()}>
+                        Confirm
+                    </Button>
+                </Modal.Footer>
+            </Modal>
+        </>
+    );
 }
 
 export default ModalDeleteUser;
--- src/services/apiService.js
+++ src/services/apiService.js
@@ -27,8 +27,13 @@ const putUpdateUser = (id, username, role, image) => {
     return axios.put('api/v1/participant', data);
 }
 
+const deleteUser = (userId) => {
+    return axios.delete('api/v1/participant', { data: {id: userId} });
+}
+
 export { 
     postCreateNewUser,
     getAllUsers,
     putUpdateUser,
+    deleteUser,
 };
