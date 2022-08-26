22928ee87474e61742eef47b6baf215f2ef37631
QuangNey <quangney12602@gmail.com>
2022-08-26 16:12:21 +0700

    build-data-before-submit

--- src/components/User/DetailQuiz.js
+++ src/components/User/DetailQuiz.js
@@ -19,7 +19,6 @@ const DetailQuiz = (props) => {
 
     const fetchQuestions = async () => {
         let res = await getDataQuiz(quizId);
-        console.log('>>> check question: ', res);
         if(res && res.EC === 0) {
             let raw = res.DT;
             let data = _.chain(raw)
@@ -40,12 +39,9 @@ const DetailQuiz = (props) => {
                     return { questionId: key, answers, questionDescription, image }
                 })
                 .value();
-            console.log(data);
             setDataQuiz(data);
         }
     }
-
-    console.log(">>> check dataQuiz: ", dataQuiz);
 
     const handlePrev = () => {
         if (index - 1 < 0) return;
@@ -73,6 +69,34 @@ const DetailQuiz = (props) => {
         if (index > -1) {
             dataQuizClone[index] = question;
             setDataQuiz(dataQuizClone);
+        }
+    }
+
+    const handleFinishQuiz = () => {
+        console.log(">>> check data before submit: ", dataQuiz)
+        let payload = {
+            quizId: +quizId,
+            answers: [],
+        };
+        let answers = [];
+        if(dataQuiz && dataQuiz.length > 0) {
+            dataQuiz.forEach(question => {
+                let questionId= question.questionId;
+                let userAnswerId = [];
+
+                question.answers.forEach(a => {
+                    if(a.isSelected === true) {
+                        userAnswerId.push(a.id);
+                    }
+                })
+                answers.push({
+                    questionId: +questionId,
+                    userAnswerId: userAnswerId
+                })
+            })
+
+            payload.answers = answers;
+            console.log("final payload: ", payload);
         }
     }
 
@@ -106,7 +130,7 @@ const DetailQuiz = (props) => {
                     onClick={() => handleNext()}
                     className="btn btn-primary">Next</button>
                     <button 
-                        onClick={() => handleNext()}
+                        onClick={() => handleFinishQuiz()}
                         className="btn btn-warning">Finish</button>
                 </div>
             </div>
